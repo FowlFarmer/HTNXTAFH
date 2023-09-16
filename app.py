9b95215091740d712657e94a10a5fd43fbe665f3
Helenessli <helenessli6@gmail.com>
2023-09-16 02:09:35 -0400

    add upload file option

--- app.py
+++ app.py
@@ -2,22 +2,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-            # Handle image upload
-            uploaded_image = request.files["image"]
+        # Handle image upload
+        uploaded_image = request.files["image"]
 
-            # Send the image to the image recognition API
-            # Extract the fruit information from the API response
+        # Send the image to the image recognition API
+        # Extract the fruit information from the API response
 
-            # Send the identified fruit to the expiry date API
-            # Extract the expiry date information from the API response
+        # Send the identified fruit to the expiry date API
+        # Extract the expiry date information from the API response
 
-            # return render_template("result.html", fruit=identified_fruit, expiry_date=expiry_date)
+        # return render_template("result.html", fruit=identified_fruit, expiry_date=expiry_date)
 
-            return render_template("index.html")
+    # If the request method is not POST or after processing POST, always render the index.html template.
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
