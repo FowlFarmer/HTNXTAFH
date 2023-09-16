c52295e96d0e68b873a12e82cd486230e82c4172
FowlFarmer <theodorez888@gmail.com>
2023-09-16 15:17:46 -0400

    Finished receipt getter

--- reciept_recognizer.py
+++ reciept_recognizer.py
@@ -5,7 +5,7 @@ import requests
 
 key = API_NINJAS_KEY
 
-print(key)
+#print(key)
 
 headers = {
     'X-Api-Key': key  # Include your API key with 'Bearer ' prefix
@@ -15,4 +15,13 @@ api_url = 'https://api.api-ninjas.com/v1/imagetotext'
 image_file_descriptor = open('receipt.jpg', 'rb')
 files = {'image': image_file_descriptor}
 r = requests.post(api_url, files=files, headers = headers)
-print(r.json())
+
+#print(r.json())
+
+long_data = r.json()
+text_blurb = ""
+for i in range(len(long_data)):
+    text_blurb += long_data[i]['text']
+    text_blurb += " "
+
+print(text_blurb)
