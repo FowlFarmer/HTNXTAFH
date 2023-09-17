7b55424a444b3c7e1e134caab5a9c1fc7ba00faf
Pancreas <108845209+Pancraes@users.noreply.github.com>
2023-09-16 23:09:28 -0400

    dfsdf

--- recipe_finder.py
+++ recipe_finder.py
@@ -6,11 +6,11 @@ co = cohere.Client(COHERE_API_KEY)
 
 
 const cohere = require('cohere-ai');
-cohere.init('XH6WEkN6940HTNO4hl1517Hpl1pX7gW8hpS3RisW'); // This is your trial API key
+cohere.init('XH6WEkN6940HTNO4hl1517Hpl1pX7gW8hpS3RisW');
 (async () => {
   const response = await cohere.generate({
     model: 'command',
-    prompt: 'give me a recipe using the ingredients: parmesan wedge, milk,  chicken wings, and banana',
+    prompt: f'give me a recipe using the ingredients: {prompt}',
     max_tokens: 2366,
     temperature: 1,
     k: 0,
