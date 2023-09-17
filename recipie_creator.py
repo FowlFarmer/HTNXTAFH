4d582d088a9cd742cf3b9559a9d4e5c1af68d81d
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2023-09-17 00:56:37 -0400

    button playing and chatgtp

--- recipie_creator.py
+++ recipie_creator.py
@@ -7,16 +7,30 @@ openai.api_key = OPENAI_KEY
 def generate_recipe(ingredients, prompt):
     ingredients_str = ', '.join(ingredients)  # Convert the list of ingredients to a comma-separated string
     prompt = f" {prompt}:{ingredients_str}"
-    response = openai.Completion.create(
-        engine="davinci",  # Choose the GPT-3 engine you want to use
-        prompt=prompt,
-        max_tokens=2000  # Adjust based on your requirements
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+        {
+            "role": "system",
+            "content": "You are a recipe generating machine. Give me a recipe that uses the following ingredients. Respond in HTML only using p and b tags. It must not have any other HTML tags. Keep it medium length and detail. Give it a title at the top. You do not have to use all ingredients, and can choose more to buy. The ingredients will follow."
+        },
+        {
+            "role": "user",
+            "content": f"{ingredients_str}"
+        },
+        ],
+        temperature=1,
+        max_tokens=2000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
+
     print(response)
-    return response.choices[0].text
+    return response.choices[0].message.content
 
 if __name__ == "__main__":
     ingredients = ["chicken", "rice", "broccoli"]
-    prompt = "You are a recipie generating machine. Give me a recipie that uses the following ingredients. Give the recipie a name, and respond in very basic HTML format with bold and headers. Keep it short. The ingredients are: "
+    prompt = "You are a recipe generating machine. Give me a recipe that uses the following ingredients. Respond in HTML only using p and b tags. It must not have any other HTML tags. Keep it medium length and detail. Give it a title at the top. You do not have to use all ingredients, and can choose more to buy. The ingredients will follow."
     recipe = generate_recipe(ingredients, prompt)
     print(recipe)
